bdce8602a745f2f58e3e8af1022c9f5b14609a16
MAITAYORMARIS <yormarism@gmail.com>
2023-07-09 18:43:12 -0300

    const controladores:agregar uno y varios, actualizar, eliminar uno, obtener uno y todos

--- models/itinerariesModel.js
+++ models/itinerariesModel.js
@@ -3,16 +3,17 @@ const mongoose = require ('mongoose')
 const itinerariesSchema = new mongoose.Schema({
     
     city_id:{type:String, required:true}, 
-    itineraryName:{type:String, required:true},
+    name:{type:String, required:true},
     collaborator: {type:String, required:true},
     profilePic: {type:String, required:true},
     price: {type:Number, required:true},
     duration: {type:String, required:true},
     hashtags: {type:String, required:true},
     likes: {type:Number, required:true},
-    pictureUno: {type:String, required:true},
-    pictureDos: {type:String, required:false},
-    pictureDos: {type:String, required:false},
+    image: {type:String, required:true},
+    origen: {type:String, required:true},
+    placesToVisit: {type:Array, required:true},
+    activities:{type:Object, required:true}
 })
 const Itineraries = mongoose.model("Itineraries", itinerariesSchema)
 module.exports = Itineraries
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express')
 require('dotenv').config()
 require('./config/database')
+const Routes = require('./routes/routes')
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
 app.set('port', PORT)
 
-app.get("/",(req,res)=>{
-    res.send("SERVIDOR PARA ITINERARIES CREADO CORRECTAMENTE")
-})
+// middlewares
+app.use(express.json())
+app.use("/api", Routes)
+
 
 app.listen(PORT,()=>{
     console.log("SERVIDOR CORRIENDO EN EL PUERTO:" + app.get('port'))
 }
 )
+
+
